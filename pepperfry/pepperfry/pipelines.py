9f03465e34c8132fe7330b47d8a115f6009f8995
varun <varunsrivastava.v@gmail.com>
2018-09-12 19:13:08 +0530

    Refactoring

--- pepperfry/pepperfry/pipelines.py
+++ pepperfry/pepperfry/pipelines.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import os
 import scrapy
 from scrapy.pipelines.images import ImagesPipeline
@@ -11,12 +7,6 @@ from shutil import move
 
 
 class PepperfryImagesPipeline(ImagesPipeline):
-    # def process_item(self, item, spider):
-        # import ipdb
-        # ipdb.set_trace()
-        # self.image_store = spider.settings.get('IMAGES_STORE')
-        # return item
-
     def item_completed(self, results, item, info):
         self.image_store = 'images_store'
         target_dir = item['item_dir']
@@ -29,9 +19,6 @@ class PepperfryImagesPipeline(ImagesPipeline):
             target_path = os.path.join(target_dir, image_name)
 
             move(path, target_path)
-
-            # if not os.rename(path, target_path):
-            #     raise DropItem("Could not move image to target folder")
 
         if self.IMAGES_RESULT_FIELD in item.fields:
             item[self.IMAGES_RESULT_FIELD] = [x for ok, x in results if ok]
